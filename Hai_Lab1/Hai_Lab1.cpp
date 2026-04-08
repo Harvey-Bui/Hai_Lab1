65780c6c23f67b973cb4920a3ad33c90a5e321ab
Harvey-Bui <builehoanghai@gmail.com>
2026-04-08 14:53:31 -0500

    Harvey changes greeting

--- Hai_Lab1/Hai_Lab1.cpp
+++ Hai_Lab1/Hai_Lab1.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello from Hai! This is my first Git commit.\n";
+    cout << "Hello from Harvey! This is my first Git commit.\n";
 
     cout << "Git makes collaboration easy!" << endl;
 
