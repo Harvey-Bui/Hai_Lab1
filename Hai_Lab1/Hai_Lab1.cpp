bc1c88c807e2be88fdfa4501c45033da171f88af
jbrasel5 <jbrasel@aum.edu>
2026-04-08 15:04:26 -0500

    update test

--- Hai_Lab1/Hai_Lab1.cpp
+++ Hai_Lab1/Hai_Lab1.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello from Hai! This is my first Git commit.\n";
+    cout << " Hello from Student B! \n";
 
     cout << "Git makes collaboration easy!" << endl;
 
